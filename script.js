501d0153b9ea3c7d9ba99e13192d65c123f5b8c5
Rhoenzfe <rhoenz.fermin@gmail.com>
2023-03-12 17:34:29 -0400

    Solved skewed deletion of rows when empty

--- script.js
+++ script.js
@@ -20,16 +20,36 @@ function removeR() {
 
 // Remove a column
 function removeC() {
-    if (numCols === 0){
-        alert("No columns exist")
-        return 0
-    }
-
     let table = document.getElementById("grid");
-    for (let i = 0; i < numRows; i++){
-        table.rows[i].deleteCell(numCols-1)
+    //get table to use and manipulate
+
+    //if there are columns then delete else do nothing and throw an alert.
+    if (numCols !== 0){
+        
+        //loop through each row to delete last cell
+        for (i = 0; i < numRows; i++) {
+            table.rows[i].deleteCell(-1);
+        }
+
+        //decrese column count
+        numCols--;
+
+        //if all columns deleted then there shouldnt be any rows
+        //delete all rows
+        if (numCols === 0) {
+            for (i = 0; i < numRows; i++) {
+                table.deleteRow(-1);
+            }
+
+            //initialize back to 0
+            numRows = 0;
+        }
+
+    }else{
+        //if trying to delete column but there is none
+        alert("No Columns to delete")
     }
-    numCols--;
+    
 }
 
 // Set global variable for selected color
